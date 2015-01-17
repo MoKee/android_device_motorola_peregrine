9c6b83bbf76403e3f717b9a2f99ce33319f19e25
martincz <martincz@mokeedev.com>
2015-01-18 03:14:18 +0800

    peregrine: mokee bringup

---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := peregrine
+PRODUCT_NAME := mk_peregrine
